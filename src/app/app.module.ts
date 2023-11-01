4d1d05267099ed93d87292868dcc04a3ca8fc754
DeveloperMateo <mateo212705@gmail.com>
2023-11-01 18:22:10 -0500

    Insercion de carpeta formulario angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list.component';
+import { ProductService } from './product.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
-  providers: [],
+  providers: [ProductService],  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
